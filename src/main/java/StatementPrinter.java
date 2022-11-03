81e5cc06aac5e09857e7fc79337922a0e859cf26
Brandon9627 <jb_yoan@ymail.com>
2022-11-03 01:08:21 +0100

    Extract 3 methods

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -6,43 +6,59 @@ public class StatementPrinter {
   public final String COMEDY = "comedy";
   public String print(Invoice invoice, Map<String, Play> plays) {
     int totalAmount = 0;
-    int volumeCredits = 0;
     StringBuilder result = new StringBuilder().append(String.format("Statement for %s\n", invoice.customer));
     NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.US);
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-
+      //calculate amount
       int thisAmount;
       switch (play.type) {
         case TRAGEDY -> {
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
+          thisAmount = calculateTragedyAmount(perf);
         }
         case COMEDY -> {
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
+          thisAmount = calculateComedyAmount(perf);
         }
-        default -> throw new Error("unknown type: ${play.type}");
+        default -> {
+          throw new Error("unknown type: ${play.type}");
+        }
       }
-
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if (COMEDY.equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
       // print line for this order.
       result.append(String.format("  %s: %s (%s seats)\n", play.name, currency.format(thisAmount / 100), perf.audience));
       totalAmount += thisAmount;
     }
     result.append(String.format("Amount owed is %s\n", currency.format(totalAmount / 100)));
-    result.append(String.format("You earned %s credits\n", volumeCredits));
+    result.append(String.format("You earned %s credits\n", calculateVolumeCredit(invoice,plays)));
     return result.toString();
   }
 
+  public int calculateVolumeCredit(Invoice invoice, Map<String, Play> plays) {
+    int credit = 0;
+    for (Performance perf : invoice.performances) {
+      Play play = plays.get(perf.playID);
+      credit += Math.max(perf.audience - 30, 0);
+      // add extra credit for every ten comedy attendees
+      if (COMEDY.equals(play.type)) credit += Math.floor(perf.audience / 5);
+    }
+    return credit;
+  }
+
+  public int calculateTragedyAmount(Performance perf) {
+    int amount = 40000;
+    if (perf.audience > 30) {
+      amount += 1000 * (perf.audience - 30);
+    }
+    return amount;
+  }
+
+  public int calculateComedyAmount(Performance perf) {
+    int amount = 30000;
+    if (perf.audience > 20) {
+      amount += 10000 + 500 * (perf.audience - 20);
+    }
+    amount += 300 * perf.audience;
+    return amount;
+  }
 }
